dce8083f843992bfa198d5ab4468f5979bad079b
Matthias Stemmler <matthias.stemmler@gmail.com>
2022-10-16 15:33:52 +0200

    Add docs and safety comments for info module

--- src/info.rs
+++ src/info.rs
@@ -1,3 +1,5 @@
+//! Methods for obtaining information on WNF states
+
 use std::ffi::c_void;
 use std::{io, mem, ptr};
 
@@ -6,11 +8,17 @@ use tracing::debug;
 use crate::ntdll;
 use crate::state::{BorrowedWnfState, OwnedWnfState, RawWnfState};
 
+/// Different classes of information on a WNF state
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 #[repr(u32)]
 enum WnfNameInfoClass {
+    /// Whether a state with a given name exists
     StateNameExist = 0,
+
+    /// Whether a state has at least one subscriber
     SubscribersPresent = 1,
+
+    /// Whether a state is "quiescent", i.e. none of the listeners subscribed to it are currently running
     IsQuiescent = 2,
 }
 
@@ -18,14 +26,18 @@ impl<T> OwnedWnfState<T>
 where
     T: ?Sized,
 {
+    /// Returns whether a WNF state with the name represented by this [`OwnedWnfState<T>`] exists
     pub fn exists(&self) -> io::Result<bool> {
         self.raw.exists()
     }
 
+    /// Returns whether this [`OwnedWnfState<T>`] has at least one subscriber
     pub fn subscribers_present(&self) -> io::Result<bool> {
         self.raw.subscribers_present()
     }
 
+    /// Returns whether this [`OwnedWnfState<T>`] is "quiescent", i.e. none of the listeners subscribed to it are
+    /// currently running
     pub fn is_quiescent(&self) -> io::Result<bool> {
         self.raw.is_quiescent()
     }
@@ -35,14 +47,18 @@ impl<T> BorrowedWnfState<'_, T>
 where
     T: ?Sized,
 {
+    /// Returns whether a WNF state with the name represented by this [`BorrowedWnfState<'a, T>`] exists
     pub fn exists(self) -> io::Result<bool> {
         self.raw.exists()
     }
 
+    /// Returns whether this [`BorrowedWnfState<'a, T>`] has at least one subscriber
     pub fn subscribers_present(self) -> io::Result<bool> {
         self.raw.subscribers_present()
     }
 
+    /// Returns whether this [`BorrowedWnfState<'a, T>`] is "quiescent", i.e. none of the listeners subscribed to it are
+    /// currently running
     pub fn is_quiescent(self) -> io::Result<bool> {
         self.raw.is_quiescent()
     }
@@ -52,22 +68,32 @@ impl<T> RawWnfState<T>
 where
     T: ?Sized,
 {
+    /// Returns whether a WNF state with the name represented by this [`RawWnfState<T>`] exists
     fn exists(self) -> io::Result<bool> {
         self.info_internal(WnfNameInfoClass::StateNameExist)
     }
 
+    /// Returns whether this [`RawWnfState<T>`] has at least one subscriber
     fn subscribers_present(self) -> io::Result<bool> {
         self.info_internal(WnfNameInfoClass::SubscribersPresent)
     }
 
+    /// Returns whether this [`RawWnfState<T>`] is "quiescent", i.e. none of the listeners subscribed to it are
+    /// currently running
     fn is_quiescent(self) -> io::Result<bool> {
         self.info_internal(WnfNameInfoClass::IsQuiescent)
     }
 
+    /// Returns the flag containing the information of the given class
     fn info_internal(self, name_info_class: WnfNameInfoClass) -> io::Result<bool> {
         let mut buffer = u32::MAX;
         let name_info_class = name_info_class as u32;
 
+        // SAFETY:
+        // - The pointer in the first argument points to a valid `u64` because it comes from a live reference
+        // - The pointer in the fifth argument is valid for writes of `u32` because it comes from a live mutable
+        //   reference
+        // - The number in the sixth argument is `4` because it equals `mem::size_of::<u32>()`
         let result = unsafe {
             ntdll::ZwQueryWnfStateNameInformation(
                 &self.state_name.opaque_value(),
--- src/ntdll.rs
+++ src/ntdll.rs
@@ -1,3 +1,13 @@
+//! Raw bindings to the WNF API in `ntdll.dll`
+//!
+//! *Note*: This is an undocumented part of the Windows API. The information given in the function documentations in
+//! this module has been collected from various sources and via reverse engineering. There is no guarantee that it is
+//! correct. This applies in particular to the safety conditions.
+//!
+//! Functions whose names start with `Rtl` (standing for "runtime library") provide higher-level abstractions while
+//! functions whose names start with `Zw` (which is just an arbitrary combination of letters) are more low level. We use
+//! a combination of both, choosing whichever function is more suitable for the task at hand.
+
 use std::ffi::c_void;
 
 use windows::{core::GUID, Win32::Foundation::NTSTATUS};
@@ -49,6 +59,29 @@ extern "system" {
         buffer_size: *mut u32,
     ) -> NTSTATUS;
 
+    /// Queries information about a WNF state name
+    ///
+    /// The information is written into a 4-byte buffer and is always a boolean value, where `0` means `false` and `1`
+    /// means `true`.
+    ///
+    /// # Arguments
+    /// - `state_name`: Pointer to the WNF state name
+    /// - `name_info_class`: Tag of the class of information to obtain
+    ///   At least the following values are valid:
+    ///   - `0`: "State name exist"
+    ///   - `1`: "Subscribers present"
+    ///   - `2`: "Is quiescent"
+    /// - `explicit_scope`: Irrelevant, can be a null pointer
+    /// - `buffer`: Pointer to a buffer the information will be written to, usually having the layout of a `u32`
+    /// - `buffer_size`: Size of the buffer in bytes, usually `4`
+    ///
+    /// # Returns
+    /// An `NTSTATUS` value that is `>= 0` on success and `< 0` on failure.
+    ///
+    /// # Safety
+    /// - `state_name` must point to a valid `u64`
+    /// - `buffer` must be valid for writes of `u32`
+    /// - `buffer_size` must be `4`
     pub(crate) fn ZwQueryWnfStateNameInformation(
         state_name: *const u64,
         name_info_class: u32,
