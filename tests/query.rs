4f4b95f1d657f89fb89e8cbb5ee8c6ec61c98506
Matthias Stemmler <matthias.stemmler@gmail.com>
2022-11-28 21:18:29 +0100

    Improve query and update tests

--- tests/query.rs
+++ tests/query.rs
@@ -1,45 +1,67 @@
 use wnf::{OpaqueData, OwnedState};
 
 #[test]
-fn get_by_value() {
+fn get() {
     let state = OwnedState::<u32>::create_temporary().unwrap();
-
     let value = 0x12345678;
     state.set(&value).unwrap();
 
     let read_value = state.get().unwrap();
+    
     assert_eq!(read_value, value);
 }
 
 #[test]
-fn get_boxed() {
-    let state = OwnedState::<u32>::create_temporary().unwrap();
+fn get_boxed_slice() {
+    let state = OwnedState::<[u32]>::create_temporary().unwrap();
+    let slice = [0x12345678, 0xABCDEF01, 0x23456789];
+    state.set(slice.as_slice()).unwrap();
 
-    let value = 0x12345678;
-    state.set(&value).unwrap();
+    let read_slice = state.get_boxed().unwrap();
 
-    let read_value = state.get_boxed().unwrap();
-    assert_eq!(*read_value, value);
+    assert_eq!(*read_slice, slice);
 }
 
 #[test]
-fn get_slice() {
+fn query() {
+    let state = OwnedState::<u32>::create_temporary().unwrap();
+    let value = 0x12345678;
+    state.set(&value).unwrap();
+
+    let (read_value, change_stamp) = state.query().unwrap().into_data_change_stamp();
+    
+    assert_eq!(read_value, value);
+    assert_eq!(change_stamp.value(), 1);
+}
+
+#[test]
+fn query_boxed_slice() {
     let state = OwnedState::<[u32]>::create_temporary().unwrap();
+    let slice = [0x12345678, 0xABCDEF01, 0x23456789];
+    state.set(slice.as_slice()).unwrap();
 
-    let values = [0x12345678, 0xABCDEF01, 0x23456789];
-    state.set(values.as_slice()).unwrap();
+    let (read_slice, change_stamp) = state.query_boxed().unwrap().into_data_change_stamp();
+    
+    assert_eq!(*read_slice, slice);
+    assert_eq!(change_stamp.value(), 1);
+}
 
-    let read_values = state.get_boxed().unwrap();
-    assert_eq!(*read_values, values);
+#[test]
+fn change_stamp() {
+    let state = OwnedState::<u32>::create_temporary().unwrap();
+
+    assert_eq!(state.change_stamp().unwrap().value(), 0);
+
+    state.set(&12345678).unwrap();
+
+    assert_eq!(state.change_stamp().unwrap().value(), 1);
 }
 
 #[test]
 fn query_opaque_data() {
     let state = OwnedState::<u32>::create_temporary().unwrap();
-    state.set(&42).unwrap();
-
+    state.set(&12345678).unwrap();
     let state: OwnedState<OpaqueData> = state.cast();
 
-    let change_stamp = state.query().unwrap().change_stamp();
-    assert_eq!(change_stamp, 1.into());
+    assert_eq!(state.query().unwrap().change_stamp().value(), 1);
 }
--- tests/update.rs
+++ tests/update.rs
@@ -3,42 +3,47 @@ use wnf::{ChangeStamp, OwnedState};
 #[test]
 fn set() {
     let state = OwnedState::<u32>::create_temporary().unwrap();
-
     let value = 0x12345678;
+
     state.set(&value).unwrap();
 
-    let read_value = state.get().unwrap();
+    let (read_value, change_stamp) = state.query().unwrap().into_data_change_stamp();
     assert_eq!(read_value, value);
+    assert_eq!(change_stamp.value(), 1);
 }
 
 #[test]
 fn set_slice() {
     let state = OwnedState::<[u32]>::create_temporary().unwrap();
+    let slice = [0x12345678, 0xABCDEF01, 0x23456789];
 
-    let values = [0x12345678, 0xABCDEF01, 0x23456789];
-    state.set(&values).unwrap();
+    state.set(&slice).unwrap();
 
-    let read_slice = state.get_boxed().unwrap();
-    assert_eq!(*read_slice, values);
+    let (read_slice, change_stamp) = state.query_boxed().unwrap().into_data_change_stamp();
+    assert_eq!(*read_slice, slice);
+    assert_eq!(change_stamp.value(), 1);
 }
 
 #[test]
-fn set_with_expected_change_stamp() {
+fn update() {
     let state = OwnedState::<u32>::create_temporary().unwrap();
+    assert_eq!(state.change_stamp().unwrap(), ChangeStamp::initial());
 
     let updated = state.update(&0x11111111, ChangeStamp::initial()).unwrap();
     assert!(updated);
-    assert_eq!(state.get().unwrap(), 0x11111111);
+    let (read_value, change_stamp) = state.query().unwrap().into_data_change_stamp();
+    assert_eq!(read_value, 0x11111111);
+    assert_eq!(change_stamp.value(), 1);
 
     let updated = state.update(&0x22222222, ChangeStamp::from(1)).unwrap();
     assert!(updated);
-    assert_eq!(state.get().unwrap(), 0x22222222);
+    let (read_value, change_stamp) = state.query().unwrap().into_data_change_stamp();
+    assert_eq!(read_value, 0x22222222);
+    assert_eq!(change_stamp.value(), 2);
 
     let updated = state.update(&0x33333333, ChangeStamp::from(1)).unwrap();
     assert!(!updated);
-    assert_eq!(state.get().unwrap(), 0x22222222);
-
-    let updated = state.update(&0x44444444, ChangeStamp::from(3)).unwrap();
-    assert!(!updated);
-    assert_eq!(state.get().unwrap(), 0x22222222);
+    let (read_value, change_stamp) = state.query().unwrap().into_data_change_stamp();
+    assert_eq!(read_value, 0x22222222);
+    assert_eq!(change_stamp.value(), 2);
 }
